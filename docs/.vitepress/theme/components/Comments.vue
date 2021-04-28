9a1c6e084ff8690985ba06e2a85e9aecaefee691
woyouwomaibu <zzzhhhwww123@gmail.com>
2021-04-28 19:15:09 +0800

    fix

--- docs/.vitepress/theme/components/Comments.vue
+++ docs/.vitepress/theme/components/Comments.vue
@@ -1,16 +1,20 @@
 <template>
-    <div id="vcomments"></div>
+    <div id="vcomments" v-show="showComment"></div>
 </template>
 
-<script setup lang="ts">
+<script setup lang="ts">    
 import Valine from 'valine'
-import { onMounted } from "vue"
-
+import { onMounted, computed } from "vue"
+import { useSiteDataByRoute, usePageData } from 'vitepress'
+const page = usePageData() 
+const showComment = computed(() => {return !page.value.frontmatter.page} )
 onMounted(() => {
     new Valine({
         el:'#vcomments',
         appId: 'eL362yYIidlk7fysgxKd0Ro7-gzGzoHsz',
-        appKey: 'gbDRdQ99EJja7ijodGSFNESp'
+        appKey: 'gbDRdQ99EJja7ijodGSFNESp',
+        path: window.location.pathname,
+        visitor: true
     })
 })
 </script>
